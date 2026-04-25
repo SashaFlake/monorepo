24b71da83d6d9fcf72b5cd1fee0af3a33c452b8e
asalnikov <asalnikov@megamarket.ru>
2026-04-25 11:19:48 +0300

    routing rules init

---
+++ service-mesh/backend/service-mesh/src/modules/routing-rules/presentation/routes.ts
@@ -0,0 +1,24 @@
+export const registryRoutes: FastifyPluginAsync<Opts> = async (app, { registry }) => {
+
+
+    app.post('/routes', async (req, reply) => {
+
+    })
+
+    app.get('/routes', async (req, reply) => {
+
+    })
+
+    app.get('/routes/:id', async (req, reply) => {
+
+    })
+
+    app.put('/routes/:id', async (req, reply) => {
+
+    })
+
+    app.delete('/routes/:id', async (req, reply) => {
+
+    })
+
+}
